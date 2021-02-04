6e69e8cf0e0a44086aea785657dea537a1e00e10
MINGYU <9832933k@hanmail.net>
2021-02-05 00:17:23 +0900

    folder changed

--- ch01/HelloCV.py
+++ ch01/HelloCV.py
@@ -2,13 +2,13 @@ import cv2
 import sys
 print('Hello, OpenCV', cv2.__version__)
 
-img = cv2.imread('googlelogo.png', cv2. IMREAD_GRAYSCALE)
+img = cv2.imread('../googlelogo.png', cv2. IMREAD_GRAYSCALE)
 
 if  img is None:
     print('Image load failed!')
     sys.exit()
 
-cv2.imwrite('googlelogogray.png', img)
+cv2.imwrite('../googlelogogray.png', img)
 
 cv2.namedWindow('image')  # 창 사이즈 조절 가능, 인자로 WiNDOW_AUTOSIZE:창 크기를 영상 크기에 맞게 변경(default), WINDOW_NORMAL:영상 크기를 창 크기에 맞게 지정
 cv2.imshow('image', img)
