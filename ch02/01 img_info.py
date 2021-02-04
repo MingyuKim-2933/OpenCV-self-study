39b9dbd86cff1dd32f6b91d7b82060f15f84866f
MINGYU <9832933k@hanmail.net>
2021-02-05 00:18:27 +0900

    Rename Files

---
+++ ch02/01 img_info.py
@@ -0,0 +1,66 @@
+'''
+영상의 속성과 픽셀 값 참조
+OpenCV는 영상 데이터를 numpy.ndarray로 표현
+
+ndim: 차원 수. 2 = grayscale, 3 = color, 4 = alpha 값이 포함된 color
+shape: 각 차원의 크기. (h, w) 또는 (h, w, 3) 등이 있다.
+size: 전체 원소 개수
+dtype: 원소의 데이터 타입. 영상 데이터는 uint8(numpy에서 지원하는 데이터타입)
+
+그레이 스케일 영상 : cv2.CV_8UC1 -> numpy.uint8, shape = (h, w)
+컬러 영상 : cv2.CV_8UC3 -> numpy.uint8, shape = (h, w, 3)
+'''
+
+import cv2, sys
+import numpy as np
+
+img1 = cv2.imread('../googlelogo.png', cv2.IMREAD_GRAYSCALE)  # shape : (h, w)
+img2 = cv2.imread('../googlelogo.png', cv2.IMREAD_COLOR)  # shape : (h, w, 3)
+
+if img1 is None or img2 is None:
+    print('Image load failed!')
+    sys.exit()
+
+print(type(img1))
+print(img1.shape)  # (1280, 1280)
+print(img2.shape)  # (1280, 1280, 3)
+print(img1.dtype)  # uint8
+print(img2.dtype)  # uint8
+
+h, w = img1.shape
+print('h x w = {} x {}'.format(w, h))
+
+h, w = img2.shape[:2]  # 컬러 영상은 슬라이싱을 사용해 앞의 두 개 값만 받아서 값에 대입해야한다.
+print('h x w = {} x {}'.format(w, h))
+
+if img1.ndim == 2:
+    print('img1 is a grayscale image')
+
+if len(img1.shape) == 2:
+    print('img1 is a grayscale image')
+
+x = 20
+y = 10
+p1 = img1[y, x]
+print(p1)
+
+p2 = img2[y, x]
+print(p2)  # [255 255 255] (B G R) 순서로 출력된다.
+
+# python에서 for문을 사용하여 이미지 처리를 하게 되면 연산속도가 느려 좋지 않다.(numpy나 opencv 라이브러리 활용하여 변경하자.)
+'''
+for y in range(h):
+    for x in range(w):
+        img1[y, x] = 0  # 검정색
+        img2[y, x] = (0, 255, 255)  # 노란색
+'''
+
+# 위와 동일한 이미지 처리을 위해 아래 방법을 사용하는 것이 낫다.
+img1[:, :] = 0  # :만 단독 사용하면 전체를 의미한다. 10까지 변경하고 싶으면 :10을 사용하자.
+img2[:, :] = (0, 255, 255)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
---
+++ ch02/03 mask_op.py
@@ -0,0 +1,51 @@
+'''
+마스크 연산과 ROI
+ROI : Region of Interest, 관심 영역 -> 영상에서 특정 연산을 수행하고자 하는 임의의 부분 영역
+
+마스크 연산 : OpenCV는 일부 함수에 대해 ROI 연산을 지원하며, 이 때 마스크 영상을 인자로 함께 전달해야 함
+(e.g.) cv2.copyTo(), cv2.calcHist(), cv2.bitwise_or(), cv2.matchTemplate(), etc.
+마스크 영상은 cv2.CV_8UC1 타입(그레이스케일 영상)
+마스크 영상의 픽셀 값이 0이 아닌 위치에서만 연산이 수행됨
+-> 보통 마스크 영상으로는 0 또는 255로 구성된 이진 영상(binary image)을 사용
+'''
+
+'''
+cv2.copyTo(src, mask, dst=None) -> dst
+- src: 입력 영상
+- mask: 마스크 영상. cv2.CV_8U.(numpy.uint8) 0이 아닌 픽셀에 대해서만 복사 연산을 수행.
+- dst: 출력 영상. 만약 src와 크기 및 타입이 같은 dst를 입력으로 지정하면 dst를 새로 생성하지 않고 연산을 수행 is None:
+       그렇지 않으면 dst를 새로 생성하여 연산을 수행한 후 반환함.
+'''
+
+import sys
+import cv2
+
+# 마스크 영상을 이용한 영상 합성
+src = cv2.imread('img/airplane.bmp', cv2.IMREAD_COLOR)
+mask = cv2.imread('img/mask_plane.bmp', cv2.IMREAD_GRAYSCALE)  # 마스크는 무조건 GRAYSCALE이여야 함
+dst = cv2.imread('img/field.bmp', cv2.IMREAD_COLOR)
+dst2 = cv2.imread('img/field.bmp', cv2.IMREAD_COLOR)
+
+cv2.copyTo(src, mask, dst)  # src, mask, dst는 모두 사이즈가 같아야 함, src와 dst는 타입이 같아야 함.
+
+# dst[mask > 0] = src[mask > 0]  # copyTo()의 결과와 같다.
+
+# cv2.imshow('src', src)
+# cv2.imshow('dst', dst)
+# cv2.imshow('mask', mask)
+
+# --------------------------------------------
+
+src2 = cv2.imread('img/opencv-logo-white.png', cv2.IMREAD_UNCHANGED)  # 알파채널이 포함된 사진은 cv2.IMREAD_UNCHANGED를 사용해야 한다.
+mask2 = src2[:, :, -1]  # 슬라이싱 부분을 잘 모르겠음.. 스터디 때 질문하기
+src2 = src2[:, :, 0:3]
+h, w = src2.shape[:2]
+crop = dst[0:h, 0:w]  # crop을 변경하면서 dst도 함께 변경된다. ( .copy()를 안썼기 때문에)
+cv2.copyTo(src2, mask2, crop)
+
+cv2.imshow('src', src2)
+cv2.imshow('crop', dst)
+cv2.imshow('mask', mask2)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
